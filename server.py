4ee6e88ddbcfbcae781436027d5b86230fd246d8
MariaEsterAlfaro <111299718+MariaEsterAlfaro@users.noreply.github.com>
2022-10-31 16:36:17 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -4,28 +4,34 @@ import csv
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def initial():
     return render_template("index.html")
 
-@app.route("/<string:page_name>") #<> allows to accept different url names, it should be indicated that it's string type
+
+# <> allows to accept different url names, it should be indicated that it's string type
+@app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
 
 def write_to_csv(data):
     with open('database.csv', newline='', mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
+        csv_writer = csv.writer(database2, delimiter=';',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
 
-#def write_to_file(data):
+# def write_to_file(data):
 #    with open('database.txt', mode='a') as database:
 #        email = data["email"]
 #        subject = data["subject"]
 #        message = data["message"]
 #        file = database.write(f'{email},{subject},{message}')
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
@@ -39,14 +45,14 @@ def submit_form():
     else:
         return "something went wrong, try again"
 
-#@app.route("/about.html")
-#def about():
+# @app.route("/about.html")
+# def about():
 #    return render_template('about.html')
 
-#@app.route("/works.html")
-#def work():
+# @app.route("/works.html")
+# def work():
 #    return render_template('works.html')
 
-#@app.route("/contact.html")
-#def contact():
+# @app.route("/contact.html")
+# def contact():
 #    return render_template('contact.html')
